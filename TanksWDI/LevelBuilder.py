f7345cb59700f68c5022b369999b8cf0a7c95a57
Wojciech Sałapatek <33402669+Wojtek2091@users.noreply.github.com>
2017-12-16 16:49:50 +0100

    Add files via upload

--- TanksWDI/LevelBuilder.py
+++ TanksWDI/LevelBuilder.py
@@ -16,10 +16,19 @@ lvlch="XXXXXXXXXXXXXXXXXXXXXXXXXX" \
 # level bulder przygotowyje lvl
 def buildLevel(B, W, H):
     bricksImg = pygame.image.load('Sprites/bricks.png')
-    playerGroup = pygame.sprite.Group([Player(1200, 600, pygame.image.load('Sprites/player.png'), 1000, 4, pygame.image.load('Sprites/bullet.png'), 1)])
-    neutralGroup, enemyGroup = buildMap("XXXXXXX  EEXXXEE  XXXXXXXX"
-                                        "XXXXXXX XXXXXXXXX XXXXXXXX"
-                                        "XXXXXXX XXXXXXXXX XXXXXXXX", B, W, H)
+    neutralGroup, enemyGroup, playerGroup = buildMap("EXXXXXX  EXXXXE   XXXXXXXE"
+                                                     "        XXXEXXXXX         "
+                                                     "        XXXXXXXXX         "
+                                                     "        XXXXXXXXX         "
+                                                     "        XXXXXXXXX         "
+                                                     "        XXXXXXXXX         "
+                                                     "        XXXXXXXXX         "
+                                                     "        XXXXXXXXX         "
+                                                     "        XXXXXXXXX         "
+                                                     "        XXXXXXXXX         "
+                                                     "        XXXXXXXXX         "
+                                                     "        XXXXXXXXX         "
+                                                     "           P              ", B, W, H)
     bulletGroup = pygame.sprite.Group([])
     boomGroup = pygame.sprite.Group([])
     groupsList = [playerGroup, neutralGroup, enemyGroup, bulletGroup, boomGroup]
@@ -33,7 +42,10 @@ def buildMap(toParse, BLOCKSIZE, WIDTH, HEIGHT):
     y = 0
     neutralGroup = pygame.sprite.Group([])
     enemyGroup = pygame.sprite.Group([])
+    playerGroup = pygame.sprite.Group([])
     bricksImg = pygame.image.load('Sprites/bricks.png')
+    enemyImg = pygame.image.load('Sprites/enemy1.png')
+    bulletImg = pygame.image.load('Sprites/bullet.png')
     for ch in toParse:
         if ch == "X":
             neutralGroup.add(NormalBricksBlock(x, y, bricksImg))
@@ -41,10 +53,12 @@ def buildMap(toParse, BLOCKSIZE, WIDTH, HEIGHT):
             neutralGroup.add(NormalBricksBlock(x, y + 25, bricksImg))
             neutralGroup.add(NormalBricksBlock(x + 25, y + 25, bricksImg))
         elif ch == "E":
-            enemyGroup.add(Enemy(x, y, pygame.image.load('Sprites/player.png'), 4, 1, pygame.image.load('Sprites/bullet.png'), 1))
+            enemyGroup.add(Enemy(x, y, enemyImg, 4, 1, bulletImg, 1))
+        elif ch == "P":
+            playerGroup.add(pygame.sprite.Group([Player(x, y, pygame.image.load('Sprites/player.png'), 1000, 4, pygame.image.load('Sprites/bullet.png'), 1)]))
         if (x + BLOCKSIZE) >= WIDTH:
             x = 0
             y += BLOCKSIZE
         else:
             x += BLOCKSIZE
-    return neutralGroup, enemyGroup
+    return neutralGroup, enemyGroup, playerGroup
